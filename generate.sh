88965bbfbc2b5c838384b49e70509072b6b3f853
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2022-04-25 13:45:20 -0400

    Separate API and documentation generation

--- generate.sh
+++ generate.sh
@@ -2,9 +2,8 @@
 
 npm ci
 
-npx swagger-typescript-api -p https://g4-dev.v1.mrcapi.net/swagger/v1/swagger.json \
+npx swagger-typescript-api -p $1 \
     --clean-output --output src --modular --axios --templates ./templates
 
 npx ctix ./tsconfig.json
 npx tsc
-npx typedoc src/index.ts
